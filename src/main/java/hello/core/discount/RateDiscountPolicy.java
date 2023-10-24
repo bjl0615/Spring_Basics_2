c32ed04fadae18e2e484c3766ff7248715befb2d
bjl0615 <bjl0615@gmail.com>
2023-10-24 10:51:45 +0900

    Change code from Java to Spring

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,16 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-
-public class RateDiscountPolicy implements DiscountPolicy{
-
-    private int discountPercent = 10;
-
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10; //10% 할인
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP) {
+        if (member.getGrade() == Grade.VIP) {
             return price * discountPercent / 100;
-        }else {
+        } else {
             return 0;
         }
     }
